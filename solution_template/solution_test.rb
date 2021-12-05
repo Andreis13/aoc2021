cca3d386fbd03d6c8832ffbc42725c905b952c5a
Andrei Istratii <andrei.istratii@gmail.com>
2021-12-05 00:41:27 -0500

    Update template test

--- solution_template/solution_test.rb
+++ solution_template/solution_test.rb
@@ -3,23 +3,35 @@ require 'minitest/autorun'
 require './solution'
 
 class SolutionTest < Minitest::Test
-  def test_example_input
+  def test_example_input_part_one
     solution = Solution.new('./example.txt')
 
     expected_part_one_output = nil
-    expected_part_two_output = nil
 
     assert_equal expected_part_one_output, solution.part_one_output
-    assert_equal expected_part_two_output, solution.part_two_output
   end
 
-  def test_full_input
+  def test_full_input_part_one
     solution = Solution.new('./input.txt')
 
     expected_part_one_output = nil
-    expected_part_two_output = nil
 
     assert_equal expected_part_one_output, solution.part_one_output
+  end
+
+  def test_example_input_part_two
+    solution = Solution.new('./example.txt')
+
+    expected_part_two_output = nil
+
+    assert_equal expected_part_two_output, solution.part_two_output
+  end
+
+  def test_full_input_part_two
+    solution = Solution.new('./input.txt')
+
+    expected_part_two_output = nil
+
     assert_equal expected_part_two_output, solution.part_two_output
   end
 end
